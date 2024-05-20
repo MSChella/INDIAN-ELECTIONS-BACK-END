5de7647cecbc57400944f8dcee37ab9e41ec7e12
MSChella <reachchellapathy.ms@gmail.com>
2024-05-20 14:49:43 +0530

    Schema cosmetic changes

--- models/user.model.js
+++ models/user.model.js
@@ -3,25 +3,25 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    firstName: { type: String, required: true, unique: true },
-    lastName: { type: String, required: true, unique: true },
-    role: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true, unique: false },
+    lastName: { type: String, required: true, unique: false },
+    role: { type: String, required: true },
     legislativeAssemblyConstituencyId: { type: mongoose.Schema.Types.ObjectId, ref: 'LegislativeAssemblyConstituency', required: true },
     districtId: { type: mongoose.Schema.Types.ObjectId, ref: 'District', required: true },
     talukId: { type: mongoose.Schema.Types.ObjectId, ref: 'Taluk', required: false },
-    sex: { type: String, required: true, unique: true },
-    age: { type: String, required: true, unique: true },
+    sex: { type: String, required: true },
+    age: { type: String, required: true },
     contactInfo: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     address: { type: String, required: true, unique: true },
     permanentAddress: { type: String, required: true, unique: true },
     temporaryAddress: { type: String, required: true, unique: true },
-    state: { type: String, required: true, unique: true },
+    state: { type: String, required: true },
     aadharNo: { type: String, required: true, unique: true },
-    fathersName: { type: String, required: true, unique: true },
-    mothersName: { type: String, required: true, unique: true },
-    spouseName: { type: String, required: true, unique: true },
-    maritalStatus: { type: String, required: true, unique: true }
+    fathersName: { type: String, required: true },
+    mothersName: { type: String, required: true },
+    spouseName: { type: String, required: true },
+    maritalStatus: { type: String, required: true }
 
 });
 
