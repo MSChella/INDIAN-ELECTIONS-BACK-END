bee0092ba5f114c88ee7c19ed16a8eaa122a66a9
MSChella <reachchellapathy.ms@gmail.com>
2024-05-20 14:17:16 +0530

    Added more schema and data model to support the client service

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require('../controller/auth.controller');
 
 router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post('/signin', authController.login);
 // router.post('/forgot-password', authController.forgetPassword);
 // router.post('/reset-password/:token', authController.resetPassword);
 // router.get('/protected-route', authController.protectedRoute);
